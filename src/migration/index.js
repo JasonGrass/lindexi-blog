9f22f2a6e8e50700dd040b3e9c405220c672a883
JasonGrass <junjiegrass@qq.com>
2024-09-24 21:49:52 +0800

    添加版权声明的处理

--- src/migration/index.js
+++ src/migration/index.js
@@ -23,7 +23,6 @@ if (!fs.existsSync(targetDir)) {
 /*
 TODO 
 图片下载
-文章末尾的声明
 评论接入
 */
 
@@ -105,6 +104,14 @@ async function getNewFilename(createTime, filename) {
   return `${prefix}-${name}`;
 }
 
+async function getContent(content) {
+  // <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。欢迎转载、使用、重新发布，但务必保留文章署名[林德熙](http://blog.csdn.net/lindexi_gd)(包含链接:http://blog.csdn.net/lindexi_gd )，不得用于商业目的，基于本文修改后的作品务必以相同的许可发布。如有任何疑问，请与我[联系](mailto:lindexi_gd@163.com)。
+  return content.replace(
+    /<a\s*rel="license"\s*.+lindexi_gd@163.com\s*[)）]\s*。\s*/g,
+    ""
+  );
+}
+
 const slugSet = new Set();
 
 async function parseOne(file) {
@@ -114,8 +121,9 @@ async function parseOne(file) {
   }
 
   const filePath = path.join(sourceDir, file);
-  const content = await fs.readFileSync(filePath, "utf8");
+  let content = await fs.readFileSync(filePath, "utf8");
 
+  content = await getContent(content);
   const title = await getPostTitle(file, content);
   const createTime = await getCreateTime(filePath, content);
   const modTime = await getModifyTime(filePath);
